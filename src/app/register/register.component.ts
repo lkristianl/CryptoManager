d86af0bba642988966526b4754253dc07499be1d
Andres Goicoechea Enrique <andres.goicoechea.enrique@alumnos.upm.es>
2021-06-06 21:09:17 +0200

    ISSUE #111 Add html & css REGISTER, Update html & css LOGIN

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,9 +13,17 @@ export class RegisterComponent implements OnInit {
     password: null,
     firstName: null,
     lastName: null,
+    acceptTerms: null,//Add
     binanceSecret: null,
     krakenSecret: null
   };
+  msgNombreUsuarioLongitud:string = 'El nombre de un usuario tiene que tener entre 3 y 20 caracteres';
+  msgEmailUsuario:string = 'El correo NO es válido';
+  msgContrasenaUsuarioLongitud:string = 'La contraseña tiene que tener entre 6 o más caracteres';
+  msgNombreLongitud:string = 'El nombre NO puede contener más que 25 caracteres';
+
+  msgCampoObligatorio:string = 'Campo obligatorio';
+  
   isSuccessful:boolean = false;
   isSignUpFailed:boolean = false;
   errorMessage:string = '';
@@ -26,9 +34,9 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { username, email, password, firstName, lastName, binanceSecret, krakenSecret } = this.form;
+    const { username, email, password, firstName, lastName, acceptTerms, binanceSecret, krakenSecret } = this.form;
 
-    this.authService.register(username, email, password, firstName, lastName, binanceSecret, krakenSecret).subscribe(
+    this.authService.register(username, email, password, firstName, lastName, binanceSecret, krakenSecret).subscribe(//Añadir acceptTerms tocar _services
       data => {
         console.log(data);//borrar este console.log
         this.isSuccessful = true;
